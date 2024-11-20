620a05e4e870ffc51cb2bdd3c76e2da0642e80ef
Gaurav Siwal <78802276+Gauravsiwal@users.noreply.github.com>
2024-11-21 00:33:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,14 +20,13 @@ def prediction(input_data):
     scale_data = scaler.transform(input_data)
     pca_data = pca.transform(scale_data)
     pred = model.predict(pca_data)[0]
-    return pred
 
-    #if pred==0:
-    #    return 'Developed'
-    #elif pred==1:
-    #    return 'Under Developed'
-    #else:
-    #    return 'Developing''''
+    if pred==0:
+        return 'Under Developed'
+    elif pred==1:
+        return 'Developed'
+    else:
+        return 'Developing''''
 
 def main():
 
